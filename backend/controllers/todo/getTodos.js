aa2123294fb4428562a388848870c2e6c86b0e9f
amit murmu <your.akmurmu6580@google.com>
2024-09-18 12:46:53 +0530

    updated getTodo.js

--- backend/controllers/todo/getTodos.js
+++ backend/controllers/todo/getTodos.js
@@ -8,16 +8,16 @@ const getTodos = async (req, res) => {
 
   const query = role === "admin" ? {} : { userId };
   try {
-    console.log("fetching...");
-    
-    const todos = await TodoModel.find(query).populate('userId');
+    const todos = await TodoModel.find(query).populate("userId");
     if (todos.length === 0) {
       res.status(200).json({ status: true, message: "No todo found" });
     }
 
     res.status(200).json({ status: true, data: todos });
   } catch (error) {
-    res.status(500).json({ status: false, error, message: "Error fetching todos" });
+    res
+      .status(500)
+      .json({ status: false, error, message: "Error fetching todos" });
   }
 };
 
